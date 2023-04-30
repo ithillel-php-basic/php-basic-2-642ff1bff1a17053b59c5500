0a064d147a1ea74e9d5e03f1db34f9e32c581f99
tsykalenko <tsykalenko.oleksii@gmail.com>
2023-04-29 21:52:38 -0700

    test_commit_2

--- index.php
+++ index.php
@@ -143,7 +143,7 @@ $task = 'backlog';
               </h3>
             </div>
             <div class="card-body connectedSortable" data-status="backlog">
-              <?php if ($task === 'backlog') : ?>
+              <?php if ($task === 'backlog')  : ?>
               <div class="card card-info card-outline" data-task-id="1">
                 <div class="card-header">
                   <h5 class="card-title">Зробити головну</h5>
